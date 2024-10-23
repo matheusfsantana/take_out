0b089b0205effbcad33e3a98c79cc2cd5904d28a
matheusfsantana <matheusfsantana99@gmail.com>
2024-10-23 13:28:51 -0300

    remove validator folder and implement cpf validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, :last_name, :cpf, presence: true
   validates :cpf, uniqueness: true
-  validates_with CpfValidator
-  
+  validate :cpf_must_be_valid
+
   before_validation :strip_cpf
 
   private
   def strip_cpf
     self.cpf = CPF.new(self.cpf).stripped
+  end
+
+  def cpf_must_be_valid
+    unless CPF.valid?(self.cpf)
+      self.errors.add :cpf, "inválido"
+    end
   end
 end
--- app/validators/cpf_validator.rb
+++
@@ -1,7 +0,0 @@
-class CpfValidator < ActiveModel::Validator
-  def validate(record)
-    unless CPF.valid?(record.cpf)
-      record.errors.add :cpf, "inválido"
-    end
-  end
-end

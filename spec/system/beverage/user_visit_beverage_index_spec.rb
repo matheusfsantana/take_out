20d2cc24def0ad669929a12dea569b66860158fc
matheusfsantana <matheusfsantana99@gmail.com>
2024-10-27 00:59:50 -0300

    fix describe name

--- spec/system/beverage/user_visit_beverage_index_spec.rb
+++ spec/system/beverage/user_visit_beverage_index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe '' do
+describe 'User visit dish index page' do
   it 'and should redirect to root_path if isnt your restaurant' do
     first_user = User.create!(email: 'joaozinho@gmail.com', password: 'password1234', name: 'Joao', last_name: 'da Silva', cpf: CPF.generate)
     first__user_restaurant = Restaurant.create!(corporate_name: 'Hot Lanches', brand_name: 'hot lanches', cnpj: CNPJ.generate,

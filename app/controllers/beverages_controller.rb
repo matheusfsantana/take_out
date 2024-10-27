602975ae6209852c1a9f3dd56fcfe3f63da076e6
matheusfsantana <matheusfsantana99@gmail.com>
2024-10-27 01:00:04 -0300

    feat: user can register beverage

--- app/controllers/beverages_controller.rb
+++ app/controllers/beverages_controller.rb
@@ -4,7 +4,25 @@ class BeveragesController < ApplicationController
     @beverages = Beverage.all
   end
 
+  def new 
+    @restaurant = user_restaurant
+    @beverage = Beverage.new
+  end
+
+  def create 
+    @restaurant = user_restaurant
+    @beverage = Beverage.new(beverage_params)
+    @beverage.restaurant = @restaurant
+
+    return redirect_to restaurant_beverages_path(@restaurant), notice: 'Nova bebida cadastrada com sucesso.' if @beverage.save
+    render :new, status: :unprocessable_entity
+  end
+
   def show
     
+  end
+
+  def beverage_params
+    params.require(:beverage).permit(:name, :description, :calories, :alcoholic, :image)
   end
 end
---
+++ spec/system/beverage/user_register_beverage_spec.rb
@@ -0,0 +1,99 @@
+require 'rails_helper'
+
+describe 'User register beverage' do
+  it 'and should see all fields' do
+    user = User.create!(email: 'joaozinho@gmail.com', password: 'password1234', name: 'Joao', last_name: 'da Silva', cpf: CPF.generate)
+    restaurant = Restaurant.create!(corporate_name: 'Hot Lanches', brand_name: 'hot lanches', cnpj: '34.651.791/0001-31',
+                                    full_address:'Rua da Hot, 721 - RJ',email:'contato@lancheshot.com', phone_number: '81987654321', user: user)
+
+
+    login_as(user)
+    visit root_path
+    click_on 'Bebidas'
+    click_on 'Cadastrar nova bebida'
+
+    expect(current_path).to eq new_restaurant_beverage_path(restaurant)
+    expect(page).to have_field 'Nome'
+    expect(page).to have_field 'Descrição'
+    expect(page).to have_field 'Calorias'
+    expect(page).to have_field 'Imagem'
+    expect(page).to have_field 'Alcoólico'
+  end
+  it 'and should create with success' do
+    user = User.create!(email: 'joaozinho@gmail.com', password: 'password1234', name: 'Joao', last_name: 'da Silva', cpf: CPF.generate)
+    restaurant = Restaurant.create!(corporate_name: 'Hot Lanches', brand_name: 'hot lanches', cnpj: '34.651.791/0001-31',
+                                    full_address:'Rua da Hot, 721 - RJ',email:'contato@lancheshot.com', phone_number: '81987654321', user: user)
+
+
+    login_as(user)
+    visit root_path
+    click_on 'Bebidas'
+    click_on 'Cadastrar nova bebida'
+
+    fill_in 'Nome', with: 'Cerveja'
+    fill_in 'Descrição', with: 'Cerveja artesanal da casa'
+    fill_in 'Calorias', with: 300
+    check  'Alcoólico'
+    attach_file "Imagem", Rails.root.join("spec/fixtures/files/test_image.png")
+    click_on 'Cadastrar bebida'
+
+    expect(page).to have_content 'Nova bebida cadastrada com sucesso.'
+    expect(current_path).to eq restaurant_beverages_path(restaurant)
+    expect(Beverage.find(1).image).to be_attached
+  end
+  it 'and should fail when mandatory fields are empty' do
+    user = User.create!(email: 'joaozinho@gmail.com', password: 'password1234', name: 'Joao', last_name: 'da Silva', cpf: CPF.generate)
+    restaurant = Restaurant.create!(corporate_name: 'Hot Lanches', brand_name: 'hot lanches', cnpj: '34.651.791/0001-31',
+                                    full_address:'Rua da Hot, 721 - RJ',email:'contato@lancheshot.com', phone_number: '81987654321', user: user)
+
+
+    login_as(user)
+    visit root_path
+    click_on 'Bebidas'
+    click_on 'Cadastrar nova bebida'
+
+    fill_in 'Nome', with: ''
+    fill_in 'Descrição', with: ''
+    click_on 'Cadastrar bebida'
+
+    expect(page).to have_content 'Nome não pode ficar em branco'
+    expect(page).to have_content 'Descrição não pode ficar em branco'
+  end
+  it 'and should fail when calories field is negative' do
+    user = User.create!(email: 'joaozinho@gmail.com', password: 'password1234', name: 'Joao', last_name: 'da Silva', cpf: CPF.generate)
+    restaurant = Restaurant.create!(corporate_name: 'Hot Lanches', brand_name: 'hot lanches', cnpj: '34.651.791/0001-31',
+                                    full_address:'Rua da Hot, 721 - RJ',email:'contato@lancheshot.com', phone_number: '81987654321', user: user)
+
+
+    login_as(user)
+    visit root_path
+    click_on 'Bebidas'
+    click_on 'Cadastrar nova bebida'
+
+    fill_in 'Nome', with: 'Cerveja'
+    fill_in 'Descrição', with: 'Cerveja artesanal da casa'
+    fill_in 'Calorias', with: -1
+    click_on 'Cadastrar bebida'
+
+    expect(page).to have_content 'Calorias não pode ser negativa'
+  end
+  it 'and should fail when file extension different of jpeg or png' do
+    user = User.create!(email: 'joaozinho@gmail.com', password: 'password1234', name: 'Joao', last_name: 'da Silva', cpf: CPF.generate)
+    restaurant = Restaurant.create!(corporate_name: 'Hot Lanches', brand_name: 'hot lanches', cnpj: '34.651.791/0001-31',
+                                    full_address:'Rua da Hot, 721 - RJ',email:'contato@lancheshot.com', phone_number: '81987654321', user: user)
+
+
+    login_as(user)
+    visit root_path
+    click_on 'Bebidas'
+    click_on 'Cadastrar nova bebida'
+
+    fill_in 'Nome', with: 'Cerveja'
+    fill_in 'Descrição', with: 'Cerveja artesanal da casa'
+    fill_in 'Calorias', with: 1
+    attach_file "Imagem", Rails.root.join("spec/fixtures/files/teste.txt")
+    click_on 'Cadastrar bebida'
+
+    expect(page).to have_content 'Imagem deve ser JPEG ou PNG'
+  end
+end
